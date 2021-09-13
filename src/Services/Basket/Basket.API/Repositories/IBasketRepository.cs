3d8cbaec8af0f5fbe29378c379e8fb5b70d1dde1
Eduardo Acosta <Eduardo.Acosta1@ey.com>
2021-09-13 17:57:07 -0300

    Basket Micro

---
+++ src/Services/Basket/Basket.API/Repositories/IBasketRepository.cs
@@ -0,0 +1,15 @@
+﻿using Basket.API.Entitties;
+using System.Threading.Tasks;
+
+namespace Basket.API.Repositories
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+
+        Task DeleteBasket(string userName);
+
+    }
+}
