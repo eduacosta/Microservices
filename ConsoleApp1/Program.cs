e6ac34fd9ea2e1ad619643303960eaf8a89c0c2f
Eduardo Acosta <Eduardo.Acosta1@ey.com>
2021-11-03 21:07:21 -0300

    grpc

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string? message =  null;
+            PrintMessage(message);
+            int length = message!.Length; // dereferencing "message"
+            Console.WriteLine("Hello World!");
+        }
+        public static bool PrintMessage([NotNullWhen(false)] string message)
+        {
+            if (!string.IsNullOrWhiteSpace(message))
+            {
+                Console.WriteLine($"{DateTime.Now}: {message}");
+                return false;
+            }
+            return true;
+        }
+    }
+}
